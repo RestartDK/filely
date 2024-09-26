f258c42599509e77370e88c02080dcd391a3df0a
R3call <thenoobyzombie@gmail.com>
2024-09-26 18:23:15 +0200

    chore: changed xlsx to format json

---
+++ build/CMakeFiles/file_converter.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_converter.dir/src/fileformat.cpp.o"
+  "CMakeFiles/file_converter.dir/src/fileformat.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/filereader.cpp.o"
+  "CMakeFiles/file_converter.dir/src/filereader.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/filewriter.cpp.o"
+  "CMakeFiles/file_converter.dir/src/filewriter.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/formatters/csvformat.cpp.o"
+  "CMakeFiles/file_converter.dir/src/formatters/csvformat.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/formatters/jpegformat.cpp.o"
+  "CMakeFiles/file_converter.dir/src/formatters/jpegformat.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/formatters/jpgformat.cpp.o"
+  "CMakeFiles/file_converter.dir/src/formatters/jpgformat.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/formatters/jsonformat.cpp.o"
+  "CMakeFiles/file_converter.dir/src/formatters/jsonformat.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/formatters/pngformat.cpp.o"
+  "CMakeFiles/file_converter.dir/src/formatters/pngformat.cpp.o.d"
+  "CMakeFiles/file_converter.dir/src/main.cpp.o"
+  "CMakeFiles/file_converter.dir/src/main.cpp.o.d"
+  "file_converter"
+  "file_converter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file_converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
