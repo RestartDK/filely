374e604841f261e39bae8671d5e8452ea0fc82c7
paulopasso <127476976+paulopasso@users.noreply.github.com>
2024-09-27 12:47:58 +0200

    fix: djusted the method signatures in FileFormat to accept non-const references for the file streams

--- include/fileformat.h
+++ include/fileformat.h
@@ -1,4 +1,4 @@
-#ifndef FILEFORMAT_H // Include guard to prevent multiple inclusions
+#ifndef FILEFORMAT_H
 #define FILEFORMAT_H
 
 #include <fstream>
@@ -6,15 +6,13 @@
 
 class FileFormat {
 public:
-  // Virtual destructor for proper cleanup in derived classes
   virtual ~FileFormat() {}
 
   // Pure virtual method to parse data from a file stream (input file)
-  virtual std::string parse(const std::ifstream &file) const = 0;
+  virtual std::string parse(std::ifstream &file) const = 0;
 
   // Pure virtual method to format data into a file stream (output file)
-  virtual std::string format(const std::ofstream &file,
-                             const std::string data) const = 0;
+  virtual std::string format(std::ofstream &file, const std::string &data) const = 0;
 };
 
 #endif
