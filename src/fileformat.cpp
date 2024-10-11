5829f6565b913b083bccdc12e91b28f4d42d8236
R3call <thenoobyzombie@gmail.com>
2024-10-11 09:19:04 +0200

    fix: fileformat inconsistency with header file

--- src/fileformat.cpp
+++ src/fileformat.cpp
@@ -1,15 +1,19 @@
 #include <fstream>
+#include <opencv2/opencv.hpp>
 #include <string>
-using namespace std;
+#include <vector>
 
-class FileFormat
-{
+class FileFormat {
 public:
   virtual ~FileFormat() {}
 
   // Will be inherited by children classes to parse into raw data
-  virtual string parse(const ifstream &file) const = 0;
+  virtual std::string parseText(std::ifstream &file) const = 0;
+  virtual std::string parseBinary(std::ifstream &file) const = 0;
 
   // Will be inherited by children classes to format raw data into a new format
-  virtual string format(const ofstream &file, const string data) const = 0;
+  virtual std::string formatText(const std::string &data,
+                                 const std::string &outputFilePath) const = 0;
+  virtual std::string formatBinary(const std::vector<uchar> &data,
+                                   const std::string &outputFilePath) const = 0;
 };
