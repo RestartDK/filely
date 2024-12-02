a3d9a520ade99c2fb6c36d6332c2869122f11044
paulopasso <127476976+paulopasso@users.noreply.github.com>
2024-12-02 23:08:07 +0100

    feat: enhancing UX of CLI, using condtional compilation

--- include/cli.h
+++ include/cli.h
@@ -4,15 +4,30 @@
 #include <string>
 
 struct CLIOptions {
-    bool showHelp = false;
+    bool showHelp;
     std::string inputFile;
     std::string outputFormat;
     std::string outputPath;
     std::string errorMessage;
 
-    bool inputFileProvided = false;
-    bool outputFormatProvided = false;
-    bool outputPathProvided = false;
+    bool inputFileProvided;
+    bool outputFormatProvided;
+    bool outputPathProvided;
+    // Default constructor (no parameters)
+    CLIOptions()
+        : showHelp(false), inputFile(""), outputFormat(""), outputPath(""), errorMessage(""),
+          inputFileProvided(false), outputFormatProvided(false), outputPathProvided(false) {}
+
+    // Constructor overloading with basic initialization
+    CLIOptions(bool help)
+        : showHelp(help), inputFile(""), outputFormat(""), outputPath(""), errorMessage(""),
+          inputFileProvided(false), outputFormatProvided(false), outputPathProvided(false) {}
+
+    // Constructor overloading with full initialization
+    CLIOptions(bool help, const std::string& input, const std::string& format,
+               const std::string& path, const std::string& error = "")
+        : showHelp(help), inputFile(input), outputFormat(format), outputPath(path), errorMessage(error),
+          inputFileProvided(!input.empty()), outputFormatProvided(!format.empty()), outputPathProvided(!path.empty()) {}
 };
 
 CLIOptions parseArguments(int argc, char* argv[]);
